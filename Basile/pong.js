914de11bba2de26fec28aebaf06dd6e07159a9bd
Basile Leroy <basile.leroy.jnm@hotmail.com>
2020-12-15 17:32:18 +0100

    BASILE: Pong: Adding scoreboard to the canvas

--- Basile/pong.js
+++ Basile/pong.js
@@ -1,6 +1,6 @@
 
 class Vector {
-    constructor(x = 2, y = 12) {
+    constructor(x = 0, y = 0) {
 
         this.x = x;
         this.y = y;
@@ -53,11 +53,7 @@ class Pong {
 
         this.ball = new Ball
         // console.log(ball);
-        this.ball.position.x = 100;
-        this.ball.position.y = 20;
 
-        this.ball.velocity.x = 100;
-        this.ball.velocity.y = 100;
 
         // Adding players to the game:
         this.Players = [
@@ -67,6 +63,9 @@ class Pong {
 
         this.Players[0].position.x = 20;
         this.Players[1].position.x = this._canvas.width - 20;
+        this.Players.forEach(player => {
+            player.position.y = this._canvas.height / 2;
+        })
 
         let lastTime;
         const callbackTime = (milliseconds) => {
@@ -77,6 +76,46 @@ class Pong {
             requestAnimationFrame(callbackTime)
         }
         callbackTime();
+
+        this.numberPixel = 5;
+        this.scoreNumbers = [
+            '111101101101111',
+            '010010010010010',
+            '111001111100111',
+            '111001111001111',
+            '101101111001001',
+            '111100111001111',
+            '111100111101111',
+            '111001001001001',
+            '111101111101111',
+            '111101111001111',
+        ].map(String => {
+            const canvas = document.createElement('canvas');
+            canvas.height = this.numberPixel * 5;
+            canvas.width = this.numberPixel * 3;
+            const canvasContext = canvas.getContext('2d');
+            canvasContext.fillStyle = '#fff';
+
+            String.split('').forEach((fill, i) => {
+                if (fill === '1') {
+                    canvasContext.fillRect(
+                        (i % 3) * this.numberPixel, 
+                        (i / 3 | 0) * this.numberPixel, 
+                        this.numberPixel, 
+                        this.numberPixel
+                    )
+                };
+            });
+            return canvas
+        })
+
+        this.Reset();
+    }
+
+    collide(Player, ball) {
+        if (Player.left < ball.right && Player.right > ball.left && Player.top < ball.bottom && Player.bottom > ball.top) {
+            ball.velocity.x = -ball.velocity.x;
+        }
     }
 
     draw() {
@@ -87,13 +126,47 @@ class Pong {
         // Telling drawRectangle to draw the ball here:
         this.drawRectangle(this.ball)
 
+        this.drawScore();
+
         // Drawing the players:
         this.Players.forEach(player => this.drawRectangle(player));
     }
 
+    drawScore = () => {
+        const alignScores = this._canvas.width / 3;
+        const numberWidth = this.numberPixel * 4;
+
+        this.Players.forEach((player, index) => {
+            const numbers = player.score.toString().split('');
+            const offsetLeft = alignScores * (index + 1) - (numberWidth * numbers.length / 2) + this.numberPixel / 2;
+            numbers.forEach((number, position) => {
+                this._canvasContext.drawImage(this.scoreNumbers[number|0], offsetLeft + position * numberWidth, 20);
+            });
+        });
+    }
+
     drawRectangle(Rectangle) {
         this._canvasContext.fillStyle = '#fff';
-        this._canvasContext.fillRect(Rectangle.position.x, Rectangle.position.y, Rectangle.size.x, Rectangle.size.y);
+        this._canvasContext.fillRect(Rectangle.left, Rectangle.top, Rectangle.size.x, Rectangle.size.y);
+    }
+
+    // When called upon, it will reset the ball to the starting point
+    Reset = () => {
+        this.ball.position.x = this._canvas.width / 2;
+        this.ball.position.y = this._canvas.height / 2;
+
+        this.ball.velocity.x = 0;
+        this.ball.velocity.y = 0;
+    }
+
+    StartBall = () => {
+
+        // "If the ball doesn't move"
+        if (this.ball.velocity.x === 0 && this.ball.velocity.y === 0) {
+
+            this.ball.velocity.x = 150 * (Math.random() > .5 ? 1 : -1);
+            this.ball.velocity.y = 200 * (Math.random() * 2 -1);
+        }
     }
 
     Update = (updateTime) => {
@@ -102,11 +175,22 @@ class Pong {
     
         // when the position of the ball goes over the x value of 0, the movement will go negative. idem dito with the Y-axis
         if (this.ball.left < 0 || this.ball.right > this._canvas.width) {
-            this.ball.velocity.x = -this.ball.velocity.x;
+            const playerID = this.ball.velocity.x < 0 | 0;
+            this.Players[playerID].score++;
+            console.log(playerID);
+
+            // Calling the reset if the ball touches the left or right side
+            this.Reset(); 
         }
+
         if (this.ball.top < 0 || this.ball.bottom > this._canvas.height) {
             this.ball.velocity.y = -this.ball.velocity.y;
         }
+
+        // Making the AI follow the ball (y-axis):
+        this.Players[1].position.y = this.ball.position.y;
+
+        this.Players.forEach(player => this.collide(player, this.ball));
 
         // Calling draw into the updating function
         this.draw();
@@ -116,3 +200,19 @@ class Pong {
 // Accessing canvas
 const canvas = document.querySelector('#pongCanvas');
 const pong = new Pong(canvas);
+
+const spaceBar = (space) => {
+    if (space.keyCode == 32) {
+
+        pong.StartBall();
+    }
+};
+
+// Adding eventListeners to move the player on the canvas:
+// TODO: finetuning the balance between player0 and mousemovement
+canvas.addEventListener('mousemove', action => {
+    pong.Players[0].position.y = action.offsetY;
+});
+
+// Keypress is not part of canvas nessessairly, better to use document instead
+document.addEventListener('keypress', spaceBar);
