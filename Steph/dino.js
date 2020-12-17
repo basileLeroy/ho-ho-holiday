f543742a5a9d74c053ad30eead7ac07187d8fcf3
Steeeeeph <stephanie.vanbockhaven@hotmail.com>
2020-12-17 14:46:48 +0100

    Add icon in readme

--- Steph/dino.js
+++ Steph/dino.js
@@ -6,12 +6,14 @@ document.addEventListener("DOMContentLoaded", ()=>{
    const gameOver = document.getElementById("gameOver");
    const scorePlayer = document.querySelector(".scorePlayer");
    const currentScore = document.getElementById("currentScore");
+   // const highScore = document.createElement('div');
+
    let isJumping = false;
    let positionDino = 0;
    let gravity = 0.9;
    let isGameOver = false;
    let score = 0;
-   let highScoreValues = [];
+   let highScore = 0;
    let startTime = new Date();
 // console.log(startTime);
 
@@ -36,11 +38,11 @@ document.addEventListener("DOMContentLoaded", ()=>{
          if (count === 15){
             clearInterval(timerID);
             let downTimerId = setInterval(function(){
-               if (count === 0) {
+               if (count === 2 ) {
                   clearInterval(downTimerId);
                   isJumping = false;
                }
-               positionDino -= 5;
+               positionDino -= 5; 
                count --;
                positionDino = positionDino * gravity;
                dino.style.bottom = positionDino +'px';      
@@ -62,7 +64,6 @@ document.addEventListener("DOMContentLoaded", ()=>{
       // TODO make random blocks not too close to eachother
       let randomTime = Math.random()*6000;
       const obstacle = document.createElement('div');
-      const highScore = document.createElement('div');
 
       if (!isGameOver) {
          obstacle.classList.add("obstacle");
@@ -76,10 +77,8 @@ document.addEventListener("DOMContentLoaded", ()=>{
          const obstaclePassed = obstaclePosition <= 0;
          if (collisionDino){
             clearInterval(timerId);
-            // highScore.classList.add("highscore");
             gameOver.innerHTML = 'Game over';
             grid.style.backgroundImage = 'none';
-            // grid.appendChild(highScore);
 
             currentScore.innerHTML = Math.round(score);
 
@@ -106,18 +105,16 @@ generateObstacles();
 
 // TODO set score
    // get seconds from first game start
-   function getScore () {   
-      score += (new Date() - startTime)/1000;
-      currentScore.innerHTML = Math.round(score);
-   
-      if (isGameOver) {
-         // TODO push to array for highscore
-         highScoreValues.push(score);
-         console.log(highScoreValues);
-      }
+   function getScore () {  
+      score += Math.round((new Date() - startTime)/1000);
+      currentScore.innerHTML = score;
    }
 // TODO when game over: clear score and log score to array for Highscore
-
+if (isGameOver) {
+   // TODO push to array for highscore
+   highScoreValues.push(score);
+   console.log(highScoreValues)
+}
 }) 
 
 
